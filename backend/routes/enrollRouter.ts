1ff90b22800a3822fe4769a9801663aab673d8fd
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-01-31 23:01:00 +0100

    Last commit for The day

--- backend/routes/enrollRouter.ts
+++ backend/routes/enrollRouter.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import enrollStudent from '../controllers/enrollmentController';
+import {enrollStudent,verifyPayment} from '../controllers/enrollmentController';
+
 
 router.post("/enroll-student/:userId/:courseId",enrollStudent);
-
+router.get("/payment-success",verifyPayment)
 export default router;
