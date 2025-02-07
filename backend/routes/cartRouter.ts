b4ed723cb8e5da1f40229f60b6e7782d67cf3cc1
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-02-07 15:39:18 +0100

    Adding to cart Functionality in the UI

--- backend/routes/cartRouter.ts
+++ backend/routes/cartRouter.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import {addToCart, removeFromCart} from '../controllers/cartController';
+import {addToCart, removeFromCart,getCarts} from '../controllers/cartController';
 import verifyToken from '../middleware/verifyToken';
 const router = express.Router();
 router.post('/add-cart/:courseId/:userId',verifyToken,addToCart);
-router.delete('/remove-cart/:courseId/:userId',verifyToken,removeFromCart)
+router.delete('/remove-cart/:courseId/:userId',verifyToken,removeFromCart);
+router.get('/carts/:userId',getCarts)
 export default router;
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { useCourseStore } from '../../store/useCourseStore'
+import { useAuthStore } from '../../store/useAuthStore';
+import { useEffect } from 'react';
 const Cart = () => {
+
+  const {authUser} = useAuthStore()as unknown as {authUser: {user:any}};
+  const {getCarts} = useCourseStore();
+  
+  const userID =  authUser?.user._id;
+  console.log(userID)
+  useEffect(()=>{
+    getCarts(userID);
+  },[])
   return (
     <div>
-      
+      <h1 className='text-3xl font-bold text-center mt-5 mb-9'>Shopping Carts </h1>
     </div>
   )
 }
